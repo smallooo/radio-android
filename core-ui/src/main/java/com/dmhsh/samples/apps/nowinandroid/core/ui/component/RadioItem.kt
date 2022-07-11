276038df0edf5f1748d9bc581c1550e71daeefc1
smallooo <donging1.he@gmail.com>
2022-07-11 09:28:21 +0800

    Add Searching Sheet

--- core-ui/src/main/java/com/dmhsh/samples/apps/nowinandroid/core/ui/component/RadioItem.kt
+++ core-ui/src/main/java/com/dmhsh/samples/apps/nowinandroid/core/ui/component/RadioItem.kt
@@ -1,6 +1,12 @@
 package com.dmhsh.samples.apps.nowinandroid.core.ui.component
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -11,16 +17,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Icon
 
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import coil.compose.rememberImagePainter
@@ -52,9 +59,11 @@ fun RadioItem(viewModel: ViewModel, stateCategories : List<Station>, onImageClic
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AnimatedListItem(station: Station, itemIndex: Int, onImageClick: (station: Station) -> Unit, onPlayClick: (station: Station) -> Unit) {
     val playbackConnection: PlaybackConnection = LocalPlaybackConnection.current
+    var expanded by remember { mutableStateOf(false) }
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.clickable {
@@ -67,7 +76,8 @@ fun AnimatedListItem(station: Station, itemIndex: Int, onImageClick: (station: S
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(55.dp)
-                .padding(4.dp).clickable {
+                .padding(4.dp)
+                .clickable {
                     onImageClick(station)
                 }
         )
@@ -90,10 +100,50 @@ fun AnimatedListItem(station: Station, itemIndex: Int, onImageClick: (station: S
             )
         }
         Icon(
-            imageVector = Icons.Default.MoreVert,
+            imageVector = if(expanded) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown,
             contentDescription = null,
             tint = Color.LightGray,
-            modifier = Modifier.padding(4.dp)
+            modifier = Modifier
+                .padding(4.dp)
+                .clickable { expanded = !expanded }
         )
     }
+
+
+    AnimatedVisibility(visible = expanded) {
+        Column(
+            horizontalAlignment = Alignment.Start
+        ) {
+
+
+            Row() {
+                listOf(station.tags).forEach{ tag ->
+                    Text(text = tag)
+                }
+            }
+
+
+            Row() {
+                Icon(imageVector = Icons.Default.Home, contentDescription = "")
+                Icon(imageVector = Icons.Default.Share, contentDescription = "")
+            }
+
+
+
+
+                Material3Card(
+                    backgroundColor = MaterialTheme.colors.background, modifier = Modifier
+                        .size(80.dp)
+                        .padding
+                            (8.dp)
+                        .animateEnterExit(
+                            enter = slideInHorizontally { it },
+                            exit = ExitTransition.None
+                        )
+                ) {
+
+                }
+        }
+    }
+
 }
