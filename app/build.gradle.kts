7a2f7b9bd252bb829fe4203c49732180e4625c2c
dongminghe <dongming1.he@gmail.com>
2022-07-19 18:37:20 +0800

    version 1.1.0 versionCode 12    add playsheet tag search function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.dmhsh.samples.apps.nowinandroid"
-        versionCode = 11
-        versionName = "1.0.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 12
+        versionName = "1.1.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         // Custom test runner to set up Hilt dependency graph
         testInstrumentationRunner = "com.dmhsh.samples.apps.nowinandroid.core.testing.NiaTestRunner"
